0dd720850ec15270c10386b95010163a64b9cad4
Daniel Majewski <d.majewski.3453@gmail.com>
2021-06-15 22:11:36 -0600

    stub project

---
+++ YelpReviews/src/main/java/net/dmaj/yelp/YelpReviewsApplication.java
@@ -0,0 +1,13 @@
+package net.dmaj.yelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpReviewsApplication.class, args);
+	}
+
+}
